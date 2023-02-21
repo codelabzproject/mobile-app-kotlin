095d0a28b1b91520999958ea894db3d83110559e
Lucas Lacerda <lucas.lacerda@zanthus.com.br>
2023-02-21 17:25:41 -0300

    config project

--- app/src/main/java/com/example/mobile_app_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/mobile_app_kotlin/MainActivity.kt
@@ -2,9 +2,8 @@ package com.example.mobile_app_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
-import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.android.FlutterActivity
 
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.embedding.engine.FlutterEngineCache
@@ -13,7 +12,7 @@ import io.flutter.embedding.engine.dart.DartExecutor
 private const val FLUTTER_ENGINE_ID = "module_flutter_engine"
 
 class MainActivity : AppCompatActivity() {
-    lateinit var flutterEngine: FlutterEngine
+    private lateinit var flutterEngine: FlutterEngine
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
