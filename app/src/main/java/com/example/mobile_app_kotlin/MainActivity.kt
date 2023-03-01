50ddbf5ff6b7d44cf4a5e0a5a81c4a2df40c1ebf
Lucas Lacerda <lucas.lacerda@zanthus.com.br>
2023-02-28 21:46:30 -0300

    build: remove integration flutter

--- app/src/main/java/com/example/mobile_app_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/mobile_app_kotlin/MainActivity.kt
@@ -3,43 +3,12 @@ package com.example.mobile_app_kotlin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 //import android.widget.Button
-//import io.flutter.embedding.android.FlutterActivity
-
-import io.flutter.embedding.engine.FlutterEngine
-//import io.flutter.embedding.engine.FlutterEngineCache
-//import io.flutter.embedding.engine.dart.DartExecutor
-
-//private const val FLUTTER_ENGINE_ID = "module_flutter_engine"
 
 class MainActivity : AppCompatActivity() {
-//    private lateinit var flutterEngine: FlutterEngine
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         supportActionBar?.hide()
-
-        // Funcao para acessar o modulo flutter
-//        flutterEngine = FlutterEngine(this)
-//
-//        flutterEngine.dartExecutor.executeDartEntrypoint(
-//            DartExecutor.DartEntrypoint.createDefault()
-//        )
-//
-//        FlutterEngineCache.getInstance()
-//            .put(FLUTTER_ENGINE_ID, flutterEngine)
-//
-//        val myButton= findViewById<Button>(R.id.my_button)
-////        myButton.setOnClickListener {
-////            startActivity(FlutterActivity.withCachedEngine(FLUTTER_ENGINE_ID).build(this))
-////        }
-//        myButton.setOnClickListener{
-//            startActivity(
-//                FlutterActivity.createDefaultIntent(this)
-//            )
-//        }
     }
-
-
 }
