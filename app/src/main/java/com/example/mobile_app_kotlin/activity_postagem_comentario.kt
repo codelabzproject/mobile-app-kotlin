5c4625a4d739ef01635abc6c5f65246bd93d8d9a
leticiaNCosta19 <leticia.costa@bandtec.com.br>
2023-04-18 19:53:39 -0300

    Inicio tela de postagem e cometários

---
+++ app/src/main/java/com/example/mobile_app_kotlin/activity_postagem_comentario.kt
@@ -0,0 +1,11 @@
+package com.example.mobile_app_kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_postagem_comentario : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_postagem_comentario)
+    }
+}
