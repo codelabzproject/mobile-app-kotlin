6479caebae99ed931a4b5f1566fbe360e987c05b
leticiaNCosta19 <leticia.costa@bandtec.com.br>
2023-04-21 20:40:38 -0300

    Inicialização da tela criar novo post

---
+++ app/src/main/java/com/example/mobile_app_kotlin/CreatePostActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mobile_app_kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreatePostActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_post)
+    }
+}
